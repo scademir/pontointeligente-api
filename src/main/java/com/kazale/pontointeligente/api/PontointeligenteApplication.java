2ee6197f5d5c6a525f81dffde012e507004e67f4
Ademir Santos <scademir@gmail.com>
2018-03-03 12:26:54 -0300

    Commit inicial dos arquivos do projeto.

---
+++ src/main/java/com/kazale/pontointeligente/api/PontointeligenteApplication.java
@@ -0,0 +1,12 @@
+package com.kazale.pontointeligente.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontointeligenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontointeligenteApplication.class, args);
+	}
+}
